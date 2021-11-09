374ec8dfe25fe52684c26744d9f3f537b9f76bae
Kyler Workman <kworkman@gpsfx.com>
2021-11-09 13:11:44 -0700

    Adds notes for console manipulation

--- Befunge/interpreter.js
+++ Befunge/interpreter.js
@@ -1,6 +1,47 @@
 
 var fs = require('fs');
-const { StringDecoder } = require('string_decoder');
+
+Reset = "\x1b[0m"
+Bright = "\x1b[1m"
+Dim = "\x1b[2m"
+Underscore = "\x1b[4m"
+Blink = "\x1b[5m"
+Reverse = "\x1b[7m"
+Hidden = "\x1b[8m"
+
+FgBlack = "\x1b[30m"
+FgRed = "\x1b[31m"
+FgGreen = "\x1b[32m"
+FgYellow = "\x1b[33m"
+FgBlue = "\x1b[34m"
+FgMagenta = "\x1b[35m"
+FgCyan = "\x1b[36m"
+FgWhite = "\x1b[37m"
+
+BgBlack = "\x1b[40m"
+BgRed = "\x1b[41m"
+BgGreen = "\x1b[42m"
+BgYellow = "\x1b[43m"
+BgBlue = "\x1b[44m"
+BgMagenta = "\x1b[45m"
+BgCyan = "\x1b[46m"
+BgWhite = "\x1b[47m"
+
+// Position the Cursor: \033[<L>;<C>H or \033[<L>;<C>f (puts the cursor at line L and column C)
+// Move the cursor up N lines: \033[<N>A
+// Move the cursor down N lines: \033[<N>B
+// Move the cursor forward N columns: \033[<N>C
+// Move the cursor backward N columns: \033[<N>D
+// Clear the screen, move to (0,0): \033[2J
+// Erase to end of line: \033[K
+// Save cursor position: \033[s
+// Restore cursor position: \033[u
+
+
+// console.log(FgGreen,BgMagenta,'test')
+// console.log('test2')
+// console.log(Reset)
+// console.log('test3')
 
 //TODO change how filename is detected, might not be last?
 
